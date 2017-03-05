80b48857caab06c89a7caf69e653c5b2437d9318
GV KRISHNA <krishgani2@gmail.com>
2017-03-05 13:46:46 +0530

    Update My_controller.php

--- controllers/My_controller.php
+++ controllers/My_controller.php
@@ -4,8 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class My_controller extends CI_Controller {
 public function __construct() {
         parent::__construct();
-        $this->load->helper('url');
-        $this->load->model('My_model', 'my_model');
+        $this->load->helper('url');       
     }
  public function index(){
     $this->load->view('form1');
